b8bab3157f2d789c675412f234dfefe8eceaf286
Hknots <Hknots99@gmail.com>
2023-03-27 09:58:26 +0200

    add update consumer models

--- generator/fint_core_consumer_generator.py
+++ generator/fint_core_consumer_generator.py
@@ -7,15 +7,18 @@ import shutil
 
 class FintCoreConsumerGenerator:
     def __init__(self, component: str,  information_model_version: str = "master"):
-        self.translator = Translator()
-        self.model_scraper = ModelScraper()
-        self.file_generator = FileGenerator()
+        self._translator = Translator()
+        self._model_scraper = ModelScraper()
+        self._file_generator = FileGenerator()
 
-        self.component = self.translator.replace(component)
-        self.domain, self.package = self.component.split("-")
-        self.model_scraper.fetch_models(information_model_version, self.domain, self.package)
-        self.models = self.model_scraper.get_main_models()
-        self.output_directory = f"./output/fint-core-consumer-{self.domain.lower()}-{self.package.lower()}/"
+        self._component = self._translator.replace(component)
+        self._domain, self.package = self._component.split("-")
+        self._model_scraper.fetch_models(information_model_version, self._domain, self.package)
+        self._models = self._model_scraper.get_main_models()
+        self._base_folder_name = f"fint-core-consumer-{self._domain.lower()}-{self.package.lower()}"
+        self.output_directory = f"./output/{self._base_folder_name}/"
+        self._model_path = "src/main/java/no/fintlabs/consumer/model/"
+        self._config_path = "src/main/java/no/fintlabs/consumer/config/"
 
     def generate_consumer(self):
         self._generate_structure()
@@ -25,6 +28,24 @@ class FintCoreConsumerGenerator:
         self._generate_link_mapper()
         self._generate_kustomize()
         self._generate_resources()
+
+    def update_consumer_models(self, path_to_consumer: str, override=False):
+        self.output_directory = self._validate_and_set_path(path_to_consumer, self._base_folder_name)
+        self._generate_model(override)
+
+    def _delete_existing_models(self):
+        existing_model_names = self._get_existing_model_names()
+
+        for folder in existing_model_names:
+            full_folder_path = os.path.join(self.output_directory, self._model_path, folder.lower())
+            if os.path.exists(full_folder_path):
+                shutil.rmtree(full_folder_path)
+
+    def _get_existing_model_names(self):
+        full_path = os.path.join(self.output_directory, self._model_path)
+        if os.path.exists(full_path):
+            return next(os.walk(full_path))[1]
+        raise ValueError(f"Path does not exist: {full_path}")
 
     def _generate_structure(self):
         source_dir = './input/static/'
@@ -38,29 +59,32 @@ class FintCoreConsumerGenerator:
     def _generate_base(self):
         source_dir = './input/base/'
         for file_name in os.listdir(source_dir):
-            self.file_generator.read_file(source_dir + file_name)
-            self.file_generator.replace_content(self.component)
-            self.file_generator.generate_file(file_name, self.output_directory)
+            self._file_generator.read_file(source_dir + file_name)
+            self._file_generator.replace_content(self._component)
+            self._file_generator.generate_file(file_name, self.output_directory)
 
-    def _generate_model(self):
+    def _generate_model(self, override=False):
+        if override:
+            self._delete_existing_models()
+
         source_dir = './input/model/'
-        for model in self.models:
-            output_dir = self.output_directory + f"src/main/java/no/fintlabs/consumer/model/{model.name.lower()}/"
-            os.makedirs(output_dir)
+        for model in self._models:
+            output_dir = os.path.join(self.output_directory, self._model_path, model.name.lower())
+            os.makedirs(output_dir, exist_ok=True)
             for file_name in os.listdir(source_dir):
-                self.file_generator.read_file(source_dir + file_name)
-                self.file_generator.replace_content(self.component, model.name)
-                self.file_generator.generate_file(file_name, output_dir, model.name)
+                self._file_generator.read_file(source_dir + file_name)
+                self._file_generator.replace_content(self._component, model.name)
+                self._file_generator.generate_file(file_name, output_dir, model.name)
 
     def _generate_rest_endpoint(self):
         source_dir = './input/config/'
-        output_dir = self.output_directory + "src/main/java/no/fintlabs/consumer/config/"
+        output_dir = os.path.join(self.output_directory, self._config_path)
         file_name = "RestEndpoints.java"
 
         with open(source_dir + file_name, "r") as file:
             contents = file.read()
 
-        for model in self.models:
+        for model in self._models:
             new_line = f'    public static final String {model.name.upper()} = "/{model.name.lower()}";'
             lines = contents.splitlines()
             for i, line in enumerate(lines):
@@ -74,14 +98,14 @@ class FintCoreConsumerGenerator:
 
     def _generate_link_mapper(self):
         source_dir = './input/config/'
-        output_dir = self.output_directory + "src/main/java/no/fintlabs/consumer/config/"
+        output_dir = os.path.join(self.output_directory, self._config_path)
         file_name = "LinkMapper.java"
 
         with open(source_dir + file_name, "r") as file:
             contents = file.read()
 
-        for model in self.models:
-            new_line = f'            .put("no.fint.model.{self.domain}.{self.package}.{model.name.capitalize()}", "/{self.domain}/{self.package}/{model.name.lower()}")'
+        for model in self._models:
+            new_line = f'            .put("no.fint.model.{self._domain}.{self.package}.{model.name.capitalize()}", "/{self._domain}/{self.package}/{model.name.lower()}")'
             lines = contents.splitlines()
             for i, line in enumerate(lines):
                 if 'return ImmutableMap.<String,String>builder()' in line:
@@ -100,15 +124,35 @@ class FintCoreConsumerGenerator:
                 if not os.path.exists(root.replace(source_dir, output_dir)):
                     os.makedirs(root.replace(source_dir, output_dir))
                 path_to_file = root + "/"
-                self.file_generator.read_file(path_to_file + filename)
-                self.file_generator.replace_content(self.component)
-                self.file_generator.generate_file(filename, path_to_file.replace(source_dir, output_dir))
+                self._file_generator.read_file(path_to_file + filename)
+                self._file_generator.replace_content(self._component)
+                self._file_generator.generate_file(filename, path_to_file.replace(source_dir, output_dir))
 
     def _generate_resources(self):
         source_dir = './input/resources/'
         output_dir = self.output_directory + 'src/main/resources/'
 
         for file in os.listdir(source_dir):
-            self.file_generator.read_file(source_dir + file)
-            self.file_generator.replace_content()
-            self.file_generator.generate_file(file, output_dir)
+            self._file_generator.read_file(source_dir + file)
+            self._file_generator.replace_content()
+            self._file_generator.generate_file(file, output_dir)
+
+    @staticmethod
+    def _validate_and_set_path(path: str, base_folder: str):
+        if os.path.isabs(path):
+            if os.path.isdir(path):
+                if os.path.basename(path) == base_folder:
+                    return path
+                elif os.path.basename(f"{path}/{base_folder}") == base_folder:
+                    return f"{path}/{base_folder}"
+                raise ValueError(f"Invalid absolute path - folder {base_folder} not found")
+            raise ValueError("Invalid absolute path - path does not exist or is not a directory")
+        else:
+            abs_path = os.path.abspath(path)
+            if os.path.isdir(abs_path):
+                if os.path.basename(abs_path) == base_folder:
+                    return abs_path
+                elif os.path.basename(f"{path}/{base_folder}") == base_folder:
+                    return f"{path}/{base_folder}"
+                raise ValueError(f"Invalid relative path - folder {base_folder} not found")
+            raise ValueError("Invalid relative path - path does not exist or is not a directory")
--- main.py
+++ main.py
@@ -3,13 +3,23 @@ from generator import FintCoreConsumerGenerator
 
 def main():
     component = input("fint-core-consumer-")
+    create_or_update = input("Create or Update: ")
     print("Leave blank for latest version")
     version = input("Information model version: ")
     if not version:
         version = "master"
 
     consumer_generator = FintCoreConsumerGenerator(component, version)
-    consumer_generator.generate_consumer()
+
+    if create_or_update.lower() in ("c", "cr", "cre", "crea", "creat", "create"):
+        return consumer_generator.generate_consumer()
+
+    path_to_consumer = input("Path: ")
+    override = input("Override Y/N: ")
+    if override.lower() in ("y", "ye", "yes"):
+        consumer_generator.update_consumer_models(path_to_consumer, override=True)
+    else:
+        consumer_generator.update_consumer_models(path_to_consumer)
 
 
 if __name__ == "__main__":
